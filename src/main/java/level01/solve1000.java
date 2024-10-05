37a8fdcc3f31ada36b455d4e861fba3d7bf36564
rustywhite404 <rustywhite404@gmail.com>
2024-10-05 15:50:16 +0900

    백준 1000번

--- src/main/java/level01/solve1000.java
+++ src/main/java/level01/solve1000.java
@@ -1,4 +1,13 @@
 package level01;
 
+import java.util.Scanner;
+
 public class solve1000 {
+    public static void main(String[] args) {
+        //백준 1000번
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+    }
 }
