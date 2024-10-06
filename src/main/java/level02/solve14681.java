22e9a291e734087733551dea0c548fc81378032d
rustywhite404 <rustywhite404@gmail.com>
2024-10-07 02:38:54 +0900

    14681

---
+++ src/main/java/level02/solve14681.java
@@ -0,0 +1,23 @@
+package level02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class solve14681 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        if(a>0&&b>0){
+            System.out.println(1);
+        }else if(a<0&&b>0){
+            System.out.println(2);
+        }else if(a<0&&b<0){
+            System.out.println(3);
+        }else {
+            System.out.println(4);
+        }
+    }
+}
