a1301aabbdbc9261b30f374791356ab4f247290a
rustywhite404 <rustywhite404@gmail.com>
2024-10-10 20:15:02 +0900

    백준2438

---
+++ src/main/java/level03/solve2438.java
@@ -0,0 +1,19 @@
+package level03;
+
+import java.io.*;
+
+public class solve2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int count = Integer.parseInt(br.readLine());
+        for(int i=1;i<=count;i++){
+            for(int j=1;j<=i;j++){
+                bw.write("*");
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
