4e462afbecfa99596a69f8b1e4c20d4f9b556d56
rustywhite404 <rustywhite404@gmail.com>
2024-10-16 15:25:07 +0900

    백준 10988

---
+++ src/main/java/level06/solv10988.java
@@ -0,0 +1,18 @@
+package level06;
+
+import java.io.*;
+public class solv10988 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        StringBuffer sb = new StringBuffer(word);
+        if(word.equals(sb.reverse().toString())){
+            bw.write(String.valueOf(1));
+        }else{
+            bw.write(String.valueOf(0));
+        }
+        bw.flush();
+        bw.close();
+    }
+}
