2eb436ace983ce4358e67d2b30e76e8620a9bdd2
rustywhite404 <rustywhite404@gmail.com>
2024-12-01 16:00:20 +0900

    백준 1715

--- src/main/java/hh99_3week/advance/센티와마법의뿅망치_배열.java
+++ src/main/java/hh99_3week/advance/센티와마법의뿅망치_배열.java
@@ -1,2 +1,51 @@
-package hh99_3week.advance;public class 센티와마법의뿅망치_배열 {
+package hh99_3week.advance;
+
+import java.util.*;
+
+public class 센티와마법의뿅망치_배열 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // 거인의 수
+        int h = sc.nextInt(); // 목표 키
+        int t = sc.nextInt(); // 뿅망치 횟수
+
+        int[] giants = new int[n];
+        for (int i = 0; i < n; i++) {
+            giants[i] = sc.nextInt();
+        }
+
+        // 뿅망치 사용
+        for (int i = 0; i < t; i++) {
+            // 현재 최댓값 찾기
+            int maxIndex = 0;
+            for (int j = 1; j < n; j++) {
+                if (giants[j] > giants[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+
+            // 가장 큰 거인의 키가 목표 이하라면 종료
+            if (giants[maxIndex] < h) {
+                System.out.println("YES");
+                System.out.println(i);
+                return;
+            }
+
+            // 뿅망치 사용
+            if (giants[maxIndex] > 1) {
+                giants[maxIndex] /= 2; // 키를 반으로 줄임
+            }
+        }
+
+        // 모든 뿅망치 사용 후 검사
+        int max = Arrays.stream(giants).max().getAsInt();
+        if (max < h) {
+            System.out.println("YES");
+            System.out.println(t);
+        } else {
+            System.out.println("NO");
+            System.out.println(max);
+        }
+    }
 }
--- src/main/java/hh99_3week/advance/카드정렬하기1715.java
+++ src/main/java/hh99_3week/advance/카드정렬하기1715.java
@@ -1,2 +1,39 @@
-package hh99_3week.advance;public class 카드정렬하기1715 {
+package hh99_3week.advance;
+
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class 카드정렬하기1715 {
+    /*
+    * 입력 :
+    * - 첫줄에 N번의 횟수가 주어진다
+    * - 둘쨋줄부터 N개의 줄에 카드 묶음의 크기가 각각 주어진다.
+    *
+    * - 풀이 :
+    * - 탐욕 알고리즘과 우선순위 큐를 사용해서 풀어야 한다.
+    * - 제일 작은 카드뭉치끼리 더하기를 반복해서 한 덩어리가 남았을 때 종료한다.
+    *
+    * - 출력 : 서로를 더해서 한 덩어리를 만들었을 때 최소값을 구해야 한다
+    *
+    * */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int total = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue();
+
+        //힙에 숫자 묶음 입력
+        for(int i=0;i<total;i++){
+            queue.add(Integer.parseInt(br.readLine()));
+        }
+        int result = 0;
+        while (queue.size()>1){
+            int sum = queue.poll()+queue.poll();
+            result += sum;
+            queue.add(sum);
+        }
+        bw.write(result+" ");
+        bw.flush();
+        bw.close();
+    }
 }
